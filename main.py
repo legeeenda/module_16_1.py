f8fa6b00562767c25c497d5a4f3be22a43270550
unknown <you@example.com>
2024-12-17 18:58:46 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, Query, Path, HTTPException
+from typing import Optional
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_main():
+    return {"message": "Главная страница"}
+
+
+@app.get("/user/admin")
+async def read_admin():
+    return {"message": "Вы вошли как администратор"}
+
+@app.get("/user")
+async def read_user_with_query(
+    username: str = Query(..., description="Имя пользователя"),  
+    age: int = Query(..., description="Возраст пользователя")   
+):
+    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+
+
+@app.get("/user/{user_id}")
+async def read_user_by_id(user_id: int = Path(..., description="ID пользователя")):
+    if user_id == "admin":  
+        raise HTTPException(status_code=404, detail="Пользователь 'admin' не допустим здесь")
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
